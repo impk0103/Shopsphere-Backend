3825c2fde4d703ff2fade7eea13196b5d81d2169
Pratik Kumar <142726515+impk0103@users.noreply.github.com>
2025-02-26 12:35:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-require("express-async-errors"); // Handles async errors automatically
 const Routes = require("./routes/route.js");
 
-dotenv.config(); // Load environment variables
+dotenv.config(); 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: process.env.FRONTEND_URL, 
+    origin: process.env.FRONTEND_URL,
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: ["Content-Type", "Authorization"]
 };
 
-const Airtable = require("airtable");
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID);
-
-
-app.use((req, res, next) => {
-    req.base = base;
-    next();
-});
+app.use(express.json());
 app.use(cors(corsOptions));
-app.use(express.json();
+app.get('/', (req, res) => {
+    res.send('Shopsphere Backend is Running!');
+  });
 app.use("/", Routes);
 
 // Start Server
